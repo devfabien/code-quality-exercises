a45da5eee8dd0c18430844ea1323f2aaf6049f98
devfabien <ish001fabi@gmail.com>
2023-10-20 12:30:21 +0200

    exercise 2 adding the self explanatory comment for the regex

--- reverseVowelsInString.js
+++ reverseVowelsInString.js
@@ -1,5 +1,7 @@
 function reverseVowels(string){
+    //Regular expression to get only the vowels which are in a string.
     const regex=/[aeiuo]/gi;
+    
     const vowels=string.match(regex)
     return string.replace(regex, ()=>vowels.pop())
 }
