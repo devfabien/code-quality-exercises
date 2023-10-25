afe36ecd2f6b8b0e9b167951c44e284a6e9954f0
devfabien <ish001fabi@gmail.com>
2023-10-25 11:16:15 +0200

    refactoring isPrime function

--- exercise4/isPrime.js
+++ exercise4/isPrime.js
@@ -1,6 +1,5 @@
 function isPrime(number) {
   if (number < 2) return false;
-  if (number == 2) return true;
 
   for (let index = 2; index < number; index++) {
     if (number % index === 0) return false;
